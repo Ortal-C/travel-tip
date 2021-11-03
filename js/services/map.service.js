c3646be3099c80ded612ed1677a9fbad393295ba
Lihi <lihise9@gmail.com>
2021-11-03 13:39:13 +0200

    Added css file, api key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,5 +1,3 @@
-
-
 export const mapService = {
     initMap,
     addMarker,
@@ -36,13 +34,12 @@ function panTo(lat, lng) {
     gMap.panTo(laLatLng);
 }
 
-
-
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
     const API_KEY = ''; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.src = "https://maps.googleapis.com/maps/api/js?key=&callback=mapReady";
+    // elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
     document.body.append(elGoogleApi);
 
