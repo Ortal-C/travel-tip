80a9bcc613f72bc4a04928b565bd3fa3ff891c75
Lihi <lihise9@gmail.com>
2021-11-03 14:27:24 +0200

    Choose place by click on the map

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -6,7 +6,7 @@ export const mapService = {
 
 var gMap;
 
-function initMap(lat = 32.0749831, lng = 34.9120554) {
+function initMap(lat = 31.91100, lng = 35.00576) {
     console.log('InitMap');
     return _connectGoogleApi()
         .then(() => {
@@ -16,7 +16,19 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 center: { lat, lng },
                 zoom: 15
             })
-            console.log('Map!', gMap);
+            console.log('Map!', gMap, gMap.center);
+            google.maps.event.addListener(gMap, "click", (event) => {
+                console.log(event.latLng);
+                var lat = event.latLng.lat().toFixed(5)
+                var lng = event.latLng.lng().toFixed(5)
+                console.log(lat, lng);
+                // var placeName = prompt('Enter place name');
+                // onAddPlace(placeName, lat, lng)
+                console.log('Map', gMap.center.lat(), gMap.center.lng());
+                // gMap.center.lat = lat;
+                // gMap.center.lng = lng;
+                // gMap.setCenter({ lat: gMap.center.lat(), lng: gMap.center.lng(), zoom: 15 })
+            });
         })
 }
 
@@ -36,7 +48,8 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    //TODO: Enter your API Key
+    // const API_KEY = 'AIzaSyCjX1gvqOY0fpuqdi5Uqny6MDtVKqOX2rU';
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = "https://maps.googleapis.com/maps/api/js?key=&callback=mapReady";
     // elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
